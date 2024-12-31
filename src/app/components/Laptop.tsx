ecfbd96130e0cb94873044e4bb0881d6566ead07
Utkarsh049 <utkarshpcs26@gmail.com>
2025-01-01 00:42:54 +0530

    added funtionality to the laptop screen while pressing the button.

--- src/app/components/Laptop.tsx
+++ src/app/components/Laptop.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import Image from 'next/image';
 
-const Laptop = () => {
-  return (
-    <div className="relative w-full max-w-[600px] mx-auto lg:my-28">
-      {/* Content Div */}
-      <div className="absolute inset-0 z-20 justify-center items-center bg-transparent text-white text-center py-8 px-12 text-[1.5rem] font-bold hidden lg:flex">
-      This hackathon is a team competition where you can have 2-5 members in your team. Most teams aim to have a mix of people with both design and developer skills and codechef is the best innovative club in vit.
-      </div>
+interface Props {
+    faqans: string
+    }
 
-      {/* Background Image */}
+const Laptop = ({faqans}: Props) => {
+
+  return (
+
+    <div className="relative w-full max-w-[600px] mx-auto lg:my-28">
+      <div className="absolute inset-0 z-20 justify-center items-center bg-transparent text-white text-center py-8 px-12 text-[1.5rem] font-bold hidden lg:flex">
+        {faqans}
+      </div>
       <Image
         src="/faqscreen.svg"
         alt="FAQ Screen"
--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,15 +1,26 @@
+'use client'
 import React from 'react'
 import Image from 'next/image'
 import FaqButtons from '../components/FaqButtons'
 import Laptop from '../components/Laptop';
+import { useState } from 'react';
 
-const page = () => {
+const Page = () => {
   const faqs: string[] = [
     'This is the best event in South East Asia, as well as Karan is the best CP',
     'Hola',
     'JP',
     'Okay'
   ];
+
+  const FAQans:string[]=[
+    "1This hackathon is a team competition where you can have 2-5 members in your team. Most teams aim to have a mix of people with both design and developer skills and codechef is the best innovative club in vit.",
+    "2This hackathon is a team competition where you can have 2-5 members in your team. Most teams aim to have a mix of people with both design and developer skills and codechef is the best innovative club in vit.",
+    "3This hackathon is a team competition where you can have 2-5 members in your team. Most teams aim to have a mix of people with both design and developer skills and codechef is the best innovative club in vit.",
+    "4This hackathon is a team competition where you can have 2-5 members in your team. Most teams aim to have a mix of people with both design and developer skills and codechef is the best innovative club in vit.",
+];
+
+const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
 
   return (
     <div className='h-screen w-screen bg-[#125A76] relative flex flex-col justify-center '>
@@ -21,13 +32,13 @@ const page = () => {
 
       <div className='flex flex-col space-y-10 p-6 w-[50%] relative z-10'>
         {faqs.map((faq, index) => (
-          <FaqButtons text={faq} key={index} />
+          <FaqButtons text={faq} key={index} onClick={()=>setSelectedIndex(index)} />
         ))}
       </div>
 
 
       <div className='h-[626.67px] w-[40%] relative z-10'>
-        <Laptop />
+      <Laptop faqans={selectedIndex !== null ? FAQans[selectedIndex] : ''} />
       </div>
         </div>
       
@@ -43,4 +54,4 @@ const page = () => {
   )
 }
 
-export default page
+export default Page
