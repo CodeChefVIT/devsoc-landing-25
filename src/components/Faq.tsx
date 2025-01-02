34cf27a4447ea435f347cb3e5640fe1b5c78133e
Utkarsh049 <utkarshpcs26@gmail.com>
2025-01-02 22:51:31 +0530

    changed the folder of faq components to components folder

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -1,10 +1,10 @@
 'use client'
 import React from 'react'
 import Image from 'next/image'
-import FaqButtons from '../faqcomponents/FaqButtons'
-import Laptop from '../faqcomponents/Laptop';
+import FaqButtons from './FaqButtons'
+import Laptop from './Laptop';
 import { useState } from 'react';
-import FaqDialog from '../faqcomponents/FaqDialog';
+import FaqDialog from './FaqDialog';
 
 const Faq = () => {
   const faqs: string[] = [
