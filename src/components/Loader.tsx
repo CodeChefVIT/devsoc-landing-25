bf2fc466b785344833ab0bccff244884bce983b8
DhruvsChopra <dhruvsinghchopra70@gmail.com>
2025-01-07 21:51:10 +0530

    Fixing reponsiveness-rive

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -4,20 +4,13 @@ import Rive, { Alignment, Fit, Layout } from "@rive-app/react-canvas";
 
 export default function Loader() {
   return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        overflow: "hidden",
-        position: "relative",
-      }}
-    >
+<div className="w-screen h-screen overflow-hidden relative bg-black">
       <Rive
         src="/devsoc.riv"
         stateMachines={["State Machine 1"]}
         artboard="Loader 2"
         layout={new Layout({
-          fit: Fit.Fill,
+          fit: Fit.FitWidth,
           alignment: Alignment.Center,
         })}
       />
