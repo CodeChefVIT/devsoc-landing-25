791935682d9f22da2de6d4fd3b70967ce3066868
Samya <mehtasamya10@gmail.com>
2025-01-10 20:29:07 +0530

    fixes #28 and #30

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,9 +5,9 @@ import placehold from '@/assets/images/placeholder.svg';
 const About = () => {
     return (
         <div id='about' className="relative z-0 bg-[url('/assets/about-bg.svg')] bg-center bg-cover bg-no-repeat min-h-screen w-full -mt-12">
-            <div className="absolute inset-0 bg-[#A85059] bg-opacity-80 z-10"></div>
+            <div className="absolute inset-0 bg-[#A85059] bg-opacity-20 z-10"></div>
             <div className="relative z-10 flex flex-col min-h-screen px-6 lg:px-16 pt-24">
-                <h1 className="lg:text-[75px] text-[45px] text-[#FF9737] text-center lg:text-left font-yerk">About</h1>
+                <h1 className="lg:text-[75px] text-[45px] text-[#FF9737] text-center lg:text-left font-yerk mb-7">About</h1>
                 <div className="flex flex-col lg:flex-row items-center lg:items-start justify-between w-full gap-8">
                     <div className="font-mono text-center lg:text-left text-[#FF9737] text-[24px] lg:text-[40px] lg:w-2/3">
                         <p>
